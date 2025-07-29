fa2d9b05d2100d2ce4f79ea745be835934358580
LENOVO <LENOVO@DESKTOP-BLCMU8F>
2025-07-29 17:24:41 +0530

    commit E13

---
+++ E13/src/test/java/Practice/NewTest.java
@@ -0,0 +1,10 @@
+package Practice;
+
+public class NewTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("hello this is github");
+	}
+
+}
